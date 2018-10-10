795dbf3bd4656bb3c133b04b2edb44b5450e87c4
Byron Roark <byron.roark@experian.com>
2018-10-10 10:42:10 -0400

    Aded ConverterTwo with a case statement

--- lib/physics/converter.ex
+++ lib/physics/converter.ex
@@ -27,7 +27,7 @@ defmodule Converter do
     (inches * 8.472522095734715723e-11) |> round_to(precision)
   end
 
-  def round_to(val, precision) when is_float(val), do
+  def round_to(val, precision \\ 5) when is_float(val) do
     Float.round(val, precision)
   end
 end
@@ -35,12 +35,13 @@ end
 defmodule ConverterTwo do
 
   def to_light_seconds({unit, val}, precision: precision) do
-    case unit do
+    light_seconds = case unit do
       :miles -> from_miles(val)
       :meters -> from_meters(val)
       :feet -> from_feet(val)
       :inches -> from_inches(val)
-    end |> round_to(precision)
+    end
+    light_seconds |> round_to(precision)
   end
 
   defp from_miles(val), do: val * 5.36819e-6
@@ -48,5 +49,5 @@ defmodule ConverterTwo do
   defp from_feet(val), do: val * 1.016702651488166404e-9
   defp from_inches(val), do: val * 8.472522095734715723e-11
   defp round_to(val, precision), do: Float.round(val, precision)
-  
+
 end
