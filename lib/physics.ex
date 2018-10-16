e43856e84fb6b9a25877819356412fcd12078455
Byron Roark <byron.roark@experian.com>
2018-10-16 00:04:09 -0400

    Refactored for orbital term and acceleration

--- lib/physics.ex
+++ lib/physics.ex
@@ -1,18 +1,3 @@
 defmodule Physics do
-  @moduledoc """
-  Documentation for Physics.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Physics.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  
 end
--- test/physics_test.exs
+++ test/physics_test.exs
@@ -1,6 +1,25 @@
 defmodule PhysicsTest do
   use ExUnit.Case
-  doctest Physics
+
+  test "escape velocity of earth is correct" do
+    ev = Physics.Rocketry.escape_velocity(:earth)
+    assert ev == 11.2
+  end
+
+  test "escape velocity of mars is correct" do
+    ev = Physics.Rocketry.escape_velocity(:mars)
+    assert ev == 5.0
+  end
+
+  test "escape velocity of moon is correct" do
+    ev = Physics.Rocketry.escape_velocity(:moon)
+    assert ev == 2.4
+  end
+
+  test "escape velocity of planet X is correct" do
+    ev = %{mass: 4.0e22, radius: 6.21e6} |> Physics.Rocketry.escape_velocity
+    assert ev == 0.9
+  end
 
   test "Converting to to_light_seconds" do
     ls = Converter.to_light_seconds({:miles, 1000}, precision: 5)
@@ -12,13 +31,14 @@ defmodule PhysicsTest do
     assert ls == 0.00537
   end
 
-  test "ConverterTwo works" do
-    ls = Converter.to_light_seconds({:miles, 1000}, precision: 5)
-    assert ls == 0.00537
+  test "Orbital aceleration for earth is 100km" do
+    orbital_acc = Physics.Rocketry.orbital_acceleration(100)
+    assert orbital_acc == 9.515619587729839
   end
 
-  test "ConverterTwo works with default values" do
-    ls = ConverterTwo.to_light_seconds({:miles, 1000})
-    assert ls == 0.00537
+  test "Orbital term for 100km above earth" do
+    term = Physics.Rocketry.orbital_term(100)
+    assert (term > 4) && (term < 5)
   end
+  
 end
